a7d6c1ddce926181f2ee4ee3855a0a1283ac29cb
archeelp <archeelparekh11@gmail.com>
2019-10-20 18:44:12 +0530

    .

--- pbl/forms.py
+++ pbl/forms.py
@@ -3,7 +3,7 @@ from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, FloatField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flaskblog.models import User
+from pbl.models import User
 
 
 class RegistrationForm(FlaskForm):
