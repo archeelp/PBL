bea7642a5b7f9137152fc5de3469d321e3202c7f
Rishiv15 <50794625+Rishiv15@users.noreply.github.com>
2020-08-06 23:21:27 +0530

    o

--- commands.py
+++ commands.py
@@ -1,11 +1,18 @@
 import click
+from flask import Blueprint
 from flask.cli import with_appcontext
 
 from pbl import db
-from pbl import models
-# from models import *
+#from pbl.models import *
 
-@click.command(name='create_tables')
-@with_appcontext
-def create_tables():
+cmd = Blueprint('db', __name__)
+
+@cmd.cli.command('create_tables') 
+def create_tables():         
     db.create_all()
+    print('***** Datebase created ****')
+
+# @click.command(name='create_tables')
+# @with_appcontext
+# def create_tables():
+#     db.create_all()
--- pbl/__init__.py
+++ pbl/__init__.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
@@ -23,5 +24,8 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'pblvjti@gmail.com'
 app.config['MAIL_PASSWORD'] = 'arch1234'
 mail = Mail(app)
+
+from commands import cmd
+app.register_blueprint(cmd)
 
 from pbl import routes
