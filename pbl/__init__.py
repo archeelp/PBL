c519f859d1826fd51c84358f1a04d15517236bf8
Rishiv15 <50794625+Rishiv15@users.noreply.github.com>
2020-08-06 22:32:03 +0530

    changed db url

--- pbl/__init__.py
+++ pbl/__init__.py
@@ -7,7 +7,9 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+# DATABASE_URL = sqlite:///site.db
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['WHOOSH_BASE'] = 'whoosh'
 db = SQLAlchemy(app)
